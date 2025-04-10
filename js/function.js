c1f77d62206b4b5e02ed0176ae5d55512682ab1f
PolBelRos <a24polbelros@inspedralbes.cat>
2025-04-10 20:12:05 +0200

    He añadido los nombres de Player1 y Player2

--- js/function.js
+++ js/function.js
@@ -18,7 +18,7 @@ function StartGame(){
 var GameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 800;
+        this.canvas.width = 1325;
         this.canvas.height = 800;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
